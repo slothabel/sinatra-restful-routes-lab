305eb8a5eef84f4bb54490e888ad59a88dc19e4f
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-09-14 15:19:13 -0400

    commit tree ea4273619a7334a15fc289b4e9ba5cd29ff27c37

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
@@ -5,5 +6,46 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
+  
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+  
+  post '/recipes' do
+    @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    redirect to "/recipes/#{@recipe.id}"
+  end
+  
+  get '/recipes/new' do
+    erb :new
+  end
+  
+  get "/recipes/:id" do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show
+  end
+
+  
+  patch '/recipes/:id' do 
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect to "/recipes/#{@recipe.id}"
+  end
+  
+  get '/recipes/:id/edit' do 
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit
+  end
+
+  delete '/recipes/:id' do 
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.delete
+    redirect to '/recipes'
+  end
+  
 
 end
